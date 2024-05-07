df9ffb5aaa33f62cc260793ac999459608a4227f
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-05-06 22:19:31 -0400

    Add files via upload

---
+++ subir6mayoweeksinLife.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+    var days= age*365;
+    console.log(`I am ${days} days old`);
+    var weeks = age*52;
+    console.log(`I am ${weeks} weeks old`);
+    var months = age*12;
+    console.log(`I am ${months} months old`);
+    /*************Don't change the code below**********/
+    }
+    
+    lifeInWeeks(24);
